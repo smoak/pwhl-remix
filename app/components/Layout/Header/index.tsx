1748e13982801efad091a56203be68d2cfaf8797
Sasha Moak <sasha.moak@gmail.com>
2024-02-29 20:33:38 -0800

    feat: add active handling for header nav links

--- app/components/Layout/Header/index.tsx
+++ app/components/Layout/Header/index.tsx
@@ -1,14 +1,29 @@
 import { Link, NavLink } from "@remix-run/react";
 import Logo from "~/components/icons/PwhlLogo";
 
+const linkClass =
+  "text-lg transition-opacity hover:opacity-70 border-b-2 border-transparent hover:border-pwhl-purple-50";
+const activeLinkClass =
+  "text-lg transition-opacity hover:opacity-70 border-b-2 border-pwhl-purple-50";
+
 export const Header = () => (
   <header className="container mx-auto flex items-center justify-between">
     <Link to="/" aria-label="Home">
       <Logo width={96} height={96} />
     </Link>
     <nav className="flex gap-6">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/standings">Standings</NavLink>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? activeLinkClass : linkClass)}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/standings"
+        className={({ isActive }) => (isActive ? activeLinkClass : linkClass)}
+      >
+        Standings
+      </NavLink>
     </nav>
   </header>
 );
