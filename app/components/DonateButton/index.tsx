b59e667dc9661ad3c86a09fb5f4868b01dd4a777
Sasha Moak <sasha.moak@gmail.com>
2024-10-20 16:21:39 -0700

    feat: add a donation button

---
+++ app/components/DonateButton/index.tsx
@@ -0,0 +1,19 @@
+export const DonateButton = () => {
+  return (
+    <div className="flex min-w-11 items-center">
+      <a
+        href="https://ko-fi.com/sashamoak"
+        target="_blank"
+        rel="noreferrer noopener external"
+        className="flex min-w-36 items-center rounded-lg bg-pwhl-purple-50 px-3 py-0.5 text-center text-white shadow"
+      >
+        <img
+          alt=""
+          src="https://storage.ko-fi.com/cdn/logomarkLogo.png"
+          className="mr-1.5 h-4 w-6 animate-kofi-wiggle"
+        />
+        Support Me!
+      </a>
+    </div>
+  );
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "kofi-wiggle": "kofi-wiggle 3s infinite",
+      },
       colors: {
         "pwhl-purple": {
           50: "#33058d",
@@ -16,6 +19,18 @@ module.exports = {
       },
       gridTemplateColumns: {
         "auto-fill": "repeat(auto-fill, minmax(320px, 1fr))",
+      },
+      keyframes: {
+        "kofi-wiggle": {
+          "0%": { transform: "rotate(0) scale(1)" },
+          "60%": { transform: "rotate(0) scale(1)" },
+          "75%": { transform: "rotate(0) scale(1.12)" },
+          "80%": { transform: "rotate(0) scale(1.1)" },
+          "84%": { transform: "rotate(-10deg) scale(1.1)" },
+          "88%": { transform: "rotate(10deg) scale(1.1)" },
+          "92%": { transform: "rotate(-10deg) scale(1.1)" },
+          "100%": { transform: "rotate(0) scale(1)" },
+        },
       },
     },
   },
